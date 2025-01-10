e724c0afe9b304a5e4dfd66805dd813907ce1e58
ignacio-montaldi <ignaciomontaldi@gmail.com>
2025-01-10 10:28:37 -0300

    Agregado cards de producto y arreglado posicion titulo del header

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardProductComponent } from "../card-product/card-product.component";
+import { SearchBarComponent } from "../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-product-list',
-  imports: [CardProductComponent],
+  imports: [CardProductComponent, SearchBarComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
